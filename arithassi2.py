64b753c4e9b0283cc4432663e9d3a31bae81d701
Harris George <68194305+deharrison74@users.noreply.github.com>
2023-11-02 23:27:24 +0530

    Add files via upload

---
+++ arithassi2.py
@@ -0,0 +1,20 @@
+# Declaring variables
+num1 = 10
+num2 = 5
+num3 = 20
+num4 = 8
+num5 = 3
+num6 = 3
+num7 = 12
+num8 = 6
+num9 = 25
+num10 = 10
+
+# Exponential
+print("exp=",num1**num4)
+
+# Modulus
+print("mod=",num3%num9)
+
+# Floor Division
+print("fdiv=",num10 // num5)
