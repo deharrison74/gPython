f1872c070ec8a6a809eaca55952e5e778e340b99
Harris George <68194305+deharrison74@users.noreply.github.com>
2023-11-04 20:39:07 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,21 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        print(x)
+
+
+x="programmer"
+print(len(x))
+
+string = 'programmer'
+length = len(string)
+print(length)
+
+print("We are \"VIKINGS\" of the north.")
+print('We are "VIKINGS" of the north.')
+
+
+# Define a list of numbers
+numbers = [23, 2, 74, 2000, 31]
+for n in numbers:
+  print(n)
